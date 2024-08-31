49f477cab6c039aea502365387240d49b3ca0da9
waradshah123 <waradshah51214@gmail.com>
2024-08-31 04:42:52 -0700

    adding square of number

--- okkkkk.py
+++ okkkkk.py
@@ -8,6 +8,9 @@ for i in range(1, 4):
     numbers.append(num)  
 total_sum = sum(numbers)
 print(f"\nThe sum of your favorite numbers is: {total_sum}")
+for num in numbers:
+   square = num**2
+print (f"\n the square of your number is :" ,{square})
 if total_sum % 2 == 0:
     print(f"{total_sum} is an even number.")
 else:
